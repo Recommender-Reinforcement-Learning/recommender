406f27a2632ace3971f54f12bb3b155ee5377286
Isaac Levy <113482582+i-levy@users.noreply.github.com>
2025-04-17 21:56:37 -0400

    Add files via upload
    Updating embedding.py and slurm script

--- embeddings.py
+++ embeddings.py
@@ -1,28 +1,44 @@
 import pandas as pd
 import numpy as np
+from scipy.sparse import csr_matrix
 from sklearn.decomposition import TruncatedSVD
 
 # Read in files
-ratings = pd.read_csv('/home/gbz6qn/Documents/MSDS/DS7540/project/ml-32m/ratings.csv')
+ratings = pd.read_csv(
+    '/home/gbz6qn/Documents/MSDS/DS7540/project/ml-32m/ratings.csv',
+    dtype={'userId': np.int32, 'movieId': np.int32, 'rating': np.float32},
+    parse_dates=['timestamp'],
+)
 movies = pd.read_csv('/home/gbz6qn/Documents/MSDS/DS7540/project/ml-32m/movies.csv')
 
 # sort by user and timestamp of rating
-ratings['timestamp'] = pd.to_datetime(ratings['timestamp'], unit='s')
 ratings = ratings.sort_values(['userId', 'timestamp'])
 
 # Create sequences
-user_sequences = ratings.groupby('userId').apply(lambda x: x[['movieId', 'rating']]).values.tolist()
+#user_sequences = ratings.groupby('userId').apply(lambda x: x[['movieId', 'rating']]).values.tolist()
 
-# Create pivoted matrix of users (sparse matrix)
-rating_matrix = ratings.pivot(index='userId', columns='movieId', values='rating').fillna(0)
+# Map userId and movieId to 0-based integer indices
+user_ids = ratings['userId'].unique()
+movie_ids = ratings['movieId'].unique()
+
+user_index = {uid: idx for idx, uid in enumerate(user_ids)}
+movie_index = {mid: idx for idx, mid in enumerate(movie_ids)}
+
+# Build sparse rating matrix (users x movies)
+row = ratings['userId'].map(user_index)
+col = ratings['movieId'].map(movie_index)
+data = ratings['rating']
+
+rating_matrix = csr_matrix((data, (row, col)), shape=(len(user_ids), len(movie_ids)))
 
 # Initialize SVD to reduce dimensionality to 50 latent features, can change this
 svd = TruncatedSVD(n_components=50)
 
-# Fit on movies and transform, save to numpy
-movie_embeddings = svd.fit_transform(rating_matrix.T)
-np.save('/home/gbz6qn/Documents/MSDS/DS7540/project/recommender/movie_embeddings.npy', movie_embeddings)
+# User embeddings: (num_users x 50)
+user_embeddings = svd.fit_transform(rating_matrix)
 
-# Transform user embeddings using fitted SVD, save to numpy
-user_embeddings = svd.transform(rating_matrix)
-np.save('/home/gbz6qn/Documents/MSDS/DS7540/project/recommender/user_embeddings.npy', movie_embeddings)
+# Movie embeddings: (num_movies x 50)
+movie_embeddings = svd.components_.T
+
+np.save('/home/gbz6qn/Documents/MSDS/DS7540/project/recommender/movie_embeddings.npy', movie_embeddings)
+np.save('/home/gbz6qn/Documents/MSDS/DS7540/project/recommender/user_embeddings.npy', user_embeddings)
